79f02da7cb1089d31c5419584c1ea485428933cd
Franco Stazzone <fralstazzone@gmail.com>
2025-01-06 20:07:22 -0300

    Initial commit

---
+++ src/main/java/io/github/fran123445/mappychan/MappychanApplication.java
@@ -0,0 +1,13 @@
+package io.github.fran123445.mappychan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappychanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappychanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fran123445/mappychan/MappychanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fran123445.mappychan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappychanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
