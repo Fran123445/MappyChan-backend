ae429292a5e758390894df7f393330c5a82bd3bb
Franco Stazzone <fralstazzone@gmail.com>
2025-01-10 20:23:07 -0300

    Adds object mapper config for autowiring

---
+++ src/main/java/io/github/fran123445/mappychan/configs/ObjectMapperConfig.java
@@ -0,0 +1,14 @@
+package io.github.fran123445.mappychan.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
