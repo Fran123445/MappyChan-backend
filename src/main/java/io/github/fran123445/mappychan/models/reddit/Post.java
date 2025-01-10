995cfb981ab56c686c545b38be977018ce26bd06
Franco Stazzone <fralstazzone@gmail.com>
2025-01-10 20:26:33 -0300

    Adds an id property

--- src/main/java/io/github/fran123445/mappychan/models/reddit/Post.java
+++ src/main/java/io/github/fran123445/mappychan/models/reddit/Post.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 public class Post {
 
+    private String id;
     private String title;
     private String body;
     private int upvotes;
     private List<Comment> comments;
 
-    public Post(String title, String body, int upvotes) {
+    public Post(String id, String title, String body, int upvotes) {
+        this.id = id;
         this.title = title;
         this.body = body;
         this.upvotes = upvotes;
