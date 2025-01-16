c33d4929cc635dba5ea569de6fdd69ca14855f95
Franco Stazzone <fralstazzone@gmail.com>
2025-01-15 21:29:45 -0300

    Removes an unecessary import

--- src/main/java/io/github/fran123445/mappychan/services/RedditAccessService.java
+++ src/main/java/io/github/fran123445/mappychan/services/RedditAccessService.java
@@ -1,6 +1,5 @@
 package io.github.fran123445.mappychan.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
